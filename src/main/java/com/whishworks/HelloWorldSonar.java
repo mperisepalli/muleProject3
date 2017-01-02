b1ab575d26670916f3df3ed3134323a4f1c29fe6
marutiperisepalli <maruti.perisepalli@whishworks.com>
2017-01-02 17:40:14 +0530

    adding sonar break code

---
+++ src/main/java/com/whishworks/HelloWorldSonar.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.whishworks;
+
+/**
+ * @author mperisepalli
+ *
+ */
+public class HelloWorldSonar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		HelloWorldSonar sonar = new HelloWorldSonar();
+		sonar.wait();
+		String name = null;
+		System.out.println("value of name is " + name.toUpperCase());
+
+	}
+
+}
